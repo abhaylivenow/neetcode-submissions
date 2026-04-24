bdaa1e71b37d946fdff5c7c4f3572960f307b0c5
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2026-04-24 19:46:53 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        int x = 0;
+        int n = strs.length;
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < strs[0].length() ; i++) {
+            char c = strs[0].charAt(i);
+            for(int j = 1 ; j < n ; j++) {
+                if(i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
