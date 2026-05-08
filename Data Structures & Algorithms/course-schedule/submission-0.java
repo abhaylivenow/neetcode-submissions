60ea823ffd961ae09e215d5af613eaaf0b371a78
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2026-05-08 21:14:51 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.java
@@ -0,0 +1,51 @@
+class Solution {
+    public boolean canFinish(int n, int[][] prerequisites) {
+        int[] ans = new int[n];
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for(int i = 0 ; i < n ; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for(int[] edge: prerequisites) {
+            int u = edge[0];
+            int v = edge[1];
+
+            graph.get(v).add(u);
+        }
+
+        int[] in = new int[n];
+
+        for(int i = 0 ; i < n ; i++) {
+            for(int nei : graph.get(i)) {
+                in[nei]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0 ; i < n ; i++) {
+            if(in[i] == 0) {
+                q.add(i);
+            }
+        }
+        List<Integer> list = new ArrayList<>();
+
+        while(!q.isEmpty()) {
+            int curr = q.poll();
+            list.add(curr);
+
+            for(int nei : graph.get(curr)) {
+                in[nei]--;
+                if(in[nei] == 0) {
+                    q.add(nei);
+                }
+            }
+        }
+
+        if(list.size() == n) {
+            return true;
+        }
+        return false;
+    }
+}
