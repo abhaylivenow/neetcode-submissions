addcb3236427a8f39ed6e1912130588d5775e1d7
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2026-05-21 22:29:38 +0530

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] table = new int[n];
+
+        for(int i = 0 ; i < n ; i++) {
+            table[i] = nums[i];
+        }
+        table[0] = nums[0];
+
+        int ans = 0;
+        for(int i = 1 ; i < n ; i++) {
+            if(i == 1) {
+                table[i] = Math.max(table[i-1], table[i]);
+            } else {
+                table[i] = Math.max(table[i-1], table[i-2] + table[i]);
+            }
+            ans = Math.max(ans, table[i]);
+        }
+        for(int i = 0 ; i < n ; i++) {
+            ans = Math.max(ans, table[i]);
+        }
+        return ans;
+    }
+}
